8cd7f7cd585470fa2a196089e74e70b692782270
chancehl <chancehlinz@gmail.com>
2025-08-21 21:41:29 -0700

    tests

--- solutions/day_one/day_one_test.py
+++ solutions/day_one/day_one_test.py
@@ -2,7 +2,6 @@ from solutions.day_one.day_one import make_lists
 
 
 def test_make_lists():
-    input = ["1   2", "3   4", "5   6"]
-    result = make_lists(input)
-
-    assert result == [[1, 3, 5], [2, 4, 6]]
+    assert make_lists(["1   2", "3   4", "5   6"]) == [[1, 3, 5], [2, 4, 6]]
+    assert make_lists([]) == [[], []]
+    assert make_lists(["1   2"]) == [[1], [2]]
