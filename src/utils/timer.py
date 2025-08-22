5abfe2eb83bce6bf80236b88eb3c0a629732d35d
chancehl <chancehlinz@gmail.com>
2025-08-21 22:16:07 -0700

    converting timer to ms

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -6,11 +6,10 @@ def time_execution(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = time.perf_counter()
-
         result = func(*args, **kwargs)
-
         end = time.perf_counter()
-        
-        print(f"{func.__name__} took {end - start:.6f} seconds")
+        elapsed_ms = (end - start) * 1000
+        print(f"{func.__name__} took {elapsed_ms:.3f}ms")
         return result
+
     return wrapper
