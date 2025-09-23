3903a9b38bac20504bc6a10785a316c73bc39d64
Chance Linz <chanlinz@amazon.com>
2025-09-23 16:10:22 -0700

    refactor: consolidate solution resolution logic into resolver.py

---
+++ aoc/resolver.py
@@ -0,0 +1,39 @@
+import importlib
+import sys
+
+
+def resolve_module(day: int):
+    module_name = f"aoc.days.{day:02d}.solution"
+
+    try:
+        return importlib.import_module(module_name)
+    except ModuleNotFoundError as ex:
+        print(f"[error] Could not load module {module_name} ({ex.msg})")
+        sys.exit(1)
+
+
+def resolve_function(module, part: int):
+    function_name = ""
+
+    if part == 1:
+        function_name = "part_one"
+    else:
+        function_name = "part_two"
+
+    try:
+        return getattr(module, function_name)
+    except AttributeError as ex:
+        print(f"[error] could not load function {function_name}(...) ({ex})")
+        sys.exit(1)
+
+
+def resolve_input(day: int, use_example: bool = False) -> str:
+    path = ""
+
+    if use_example:
+        path = f"./aoc/days/{day:02d}/example.txt"
+    else:
+        path = f"./aoc/days/{day:02d}/input.txt"
+
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
