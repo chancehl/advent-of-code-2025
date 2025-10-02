2addf2dc30f841a87c2365f06a79d79742c38777
Chance Linz <chanlinz@amazon.com>
2025-10-02 09:28:58 -0700

    click-ifying things

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -1,19 +1,25 @@
-import sys
 import math
+import os
+import pathlib
+import sys
+import click
+import dotenv
+import requests
 
 from aoc.resolver import resolve_function, resolve_input, resolve_module
-from aoc.constants import CHRISTMAS_TREE
+from aoc.constants import CHRISTMAS_TREE, SOLUTION_TEMPLATE
 
 
-def main():
-    if len(sys.argv) < 3:
-        print("usage: uv run -m aoc <day> <part> (--example)")
-        sys.exit(1)
+@click.group()
+def cli():
+    pass
 
-    day = int(sys.argv[1])
-    part = int(sys.argv[2])
-    example = bool(len(sys.argv) > 3 and sys.argv[3] == "--example")
 
+@cli.command()
+@click.option("--day", type=int, required=True, help="Day number (1-25)")
+@click.option("--part", type=int, required=True, help="Part number (1-2)")
+@click.option("--example", is_flag=True, help="Use example input")
+def execute(day, part, example):
     if day < 1 or day > 25:
         raise ValueError("day argument must be between 1 and 25")
 
@@ -22,13 +28,64 @@ def main():
 
     module = resolve_module(day)
     solution = resolve_function(module, part)
-    input = resolve_input(day, example)
+    input_data = resolve_input(day, example)
 
-    (result, elapsed) = solution(input)
+    (result, elapsed) = solution(input_data)
 
-    print("🎄Advent of Code 2025 🎄" + "\n" + CHRISTMAS_TREE)
-    print(f"[year 2025 / day {day} / part {part}]: {result} ({math.floor(elapsed)}ms)")
+    click.echo("🎄Advent of Code 2025 🎄" + "\n" + CHRISTMAS_TREE)
+    click.echo(
+        f"[year 2025 / day {day} / part {part}]: {result} ({math.floor(elapsed)}ms)"
+    )
+
+
+@cli.command()
+@click.option("--day", type=int, required=True, help="Day number (1-25)")
+@click.pass_context
+def generate(ctx, day):
+    if not (1 <= day <= 25):
+        click.echo("day must be between 1 and 25")
+        sys.exit(1)
+
+    day_dir = pathlib.Path(f"./aoc/solutions/day{day:02d}")
+
+    if day_dir.exists():
+        raise FileExistsError(f"Directory already exists: {day_dir}")
+
+    day_dir.mkdir(parents=True)
+
+    solution_file = day_dir / "solution.py"
+    solution_file.write_text(SOLUTION_TEMPLATE)
+
+    example_file = day_dir / "example.txt"
+    example_file.touch()
+
+    click.echo(
+        f"🎁 Generated placeholder solution files for day {day}. You may run the solution by executing this command in your terminal:"
+    )
+    click.echo(f"uv run -m aoc execute --day {day} --part 1 --example")
+    click.echo("🎅 Good luck!")
+
+
+@cli.command()
+@click.option("--day", type=int, required=True, help="Day number (1-25)")
+@click.pass_context
+def fetch(ctx, day):
+    dotenv.load_dotenv()
+
+    session = os.getenv("AOC_SESSION")
+    if session is None or session == "":
+        click.echo("missing AOC_SESSION environment variable")
+        sys.exit(1)
+
+    url = f"https://adventofcode.com/2024/day/{day}/input"
+
+    resp = requests.get(url, cookies={"session": session})
+    if resp.status_code != 200:
+        click.echo(f"failed to fetch input: {resp.status_code} {resp.text}")
+        ctx.exit(1)
+
+    click.echo(resp.text)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
--- aoc/constants.py
+++ aoc/constants.py
@@ -9,3 +9,16 @@ CHRISTMAS_TREE = """
        | |
        |_|
 """
+
+SOLUTION_TEMPLATE = """from aoc.common.timer import timed
+
+
+@timed
+def part_one(input_txt: str) -> int:
+    return -1
+
+
+@timed
+def part_two(input_txt: str) -> int:
+    return -1
+"""
