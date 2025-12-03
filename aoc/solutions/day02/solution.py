b99ac670396a8736a2b100102874f89ede75574e
Chance Linz <chancehl@Chances-MacBook-Pro.local>
2025-12-02 22:11:53 -0800

    adding day 2 stub

---
+++ aoc/solutions/day02/solution.py
@@ -0,0 +1,13 @@
+from aoc.common.timer import timed
+
+
+@timed
+def part_one(input_txt: str) -> int:
+    print(input_txt)
+
+    return -1
+
+
+@timed
+def part_two(input_txt: str) -> int:
+    return -1
