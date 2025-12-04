010b24a881183a002ddaa0c6bd41ee7cded91d06
Chance Linz <chancehlinz@gmail.com>
2025-12-04 13:13:18 -0800

    updating launch.json and solving day 3 part 1/2

--- aoc/solutions/day03/solution.py
+++ aoc/solutions/day03/solution.py
@@ -8,20 +8,41 @@ def part_one(input_txt: str) -> int:
     for line in input_txt.splitlines():
         batteries = [int(battery) for battery in list(line)]
 
-        max_value = max(batteries[0 : len(batteries) - 1])
-        max_index = batteries.index(max_value)
-
-        local_max = -1
-
-        for index in range(max_index + 1, len(batteries)):
-            local_max = max(local_max, batteries[index])
-
-        battery_joltage = int(f"{max_value}{local_max}")
-        joltage += battery_joltage
+        jolts = greedily_pick_largest(batteries, 2)
+        joltage += jolts
 
     return joltage
 
 
 @timed
 def part_two(input_txt: str) -> int:
-    return -1
+    joltage = 0
+
+    for line in input_txt.splitlines():
+        batteries = [int(battery) for battery in list(line)]
+
+        jolts = greedily_pick_largest(batteries, 12)
+        joltage += jolts
+
+    return joltage
+
+
+def greedily_pick_largest(nums: list[int], n: int) -> int:
+    chosen = ""
+
+    while n > 0:
+        # you can only select certain batteries here...
+        # example 8119, n = 2. largest is technically 9
+        # but that's not a valid choice because there's not
+        # anoter digit following it.
+        (start, end) = (0, len(nums)) if n == 1 else (0, len(nums) - (n - 1))
+
+        value = max(nums[start:end])
+        index = nums.index(value)
+
+        chosen = chosen + str(value)
+
+        nums = nums[index + 1 :]
+        n -= 1
+
+    return int(chosen)
