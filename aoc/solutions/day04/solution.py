38a230ea4808a4a7b5822dcabedadea026f38951
Chance Linz <chancehlinz@gmail.com>
2025-12-05 08:48:21 -0800

    cleaning up day 4

--- aoc/solutions/day04/solution.py
+++ aoc/solutions/day04/solution.py
@@ -1,4 +1,5 @@
 from aoc.common.timer import timed
+from aoc.common.grid import NEIGHBORS
 
 type WrappingPaperGrid = list[list[str]]
 type Coordinates = tuple[int, int]
@@ -7,31 +8,31 @@ type Coordinates = tuple[int, int]
 @timed
 def part_one(input_txt: str) -> int:
     grid = make_grid(input_txt)
-
-    movable_rolls = 0
-
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[col][row] == "@" and is_movable_roll(grid, (row, col)):
-                movable_rolls += 1
-
-    return movable_rolls
+    return sum(
+        1
+        for row in range(len(grid))
+        for col in range(len(grid[0]))
+        if grid[col][row] == "@" and is_movable_roll(grid, (row, col))
+    )
 
 
 @timed
 def part_two(input_txt: str) -> int:
     grid = make_grid(input_txt)
-
     count = 0
 
     while True:
-        removable_rolls = get_removable_rolls(grid)
+        removable_rolls = [
+            (row, col)
+            for row in range(len(grid))
+            for col in range(len(grid[0]))
+            if grid[col][row] == "@" and is_movable_roll(grid, (row, col))
+        ]
 
-        if len(removable_rolls) == 0:
+        if not removable_rolls:
             break
 
         count += len(removable_rolls)
-
         for row, col in removable_rolls:
             grid[col][row] = "."
 
@@ -39,60 +40,19 @@ def part_two(input_txt: str) -> int:
 
 
 def make_grid(input: str) -> WrappingPaperGrid:
-    grid = []
-
-    lines = input.splitlines()
-
-    for line in lines:
-        grid.append(list(line))
-
-    return grid
+    return [list(line) for line in input.splitlines()]
 
 
 def is_movable_roll(grid: WrappingPaperGrid, coords: Coordinates) -> bool:
-    surrounding_rolls = 0
-
     row, col = coords
-
-    left = (row - 1, col)
-    right = (row + 1, col)
-
-    up = (row, col - 1)
-    down = (row, col + 1)
-
-    up_left = (row - 1, col - 1)
-    up_right = (row + 1, col - 1)
-
-    down_right = (row + 1, col + 1)
-    down_left = (row - 1, col + 1)
-
-    to_check = [left, right, up, down, up_left, up_right, down_right, down_left]
-
-    for node in to_check:
-        if get_node(grid, node) == "@":
-            surrounding_rolls += 1
-
-            if surrounding_rolls >= 4:
-                return False
-
-    return True
+    surrounding_count = sum(
+        1 for dr, dc in NEIGHBORS if get_node(grid, (row + dr, col + dc)) == "@"
+    )
+    return surrounding_count < 4
 
 
 def get_node(grid: WrappingPaperGrid, coords: Coordinates) -> str | None:
     row, col = coords
-
-    if row < 0 or row > len(grid[0]) - 1 or col < 0 or col > len(grid) - 1:
-        return None
-
-    return grid[col][row]
-
-
-def get_removable_rolls(grid: WrappingPaperGrid) -> list[Coordinates]:
-    coords = []
-
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[col][row] == "@" and is_movable_roll(grid, (row, col)):
-                coords.append((row, col))
-
-    return coords
+    if 0 <= row < len(grid[0]) and 0 <= col < len(grid):
+        return grid[col][row]
+    return None
