d7a3d41dae683e70c2c8482c1e3b8f1ee6af0cbc
Chance Linz <chancehlinz@gmail.com>
2025-12-05 10:06:58 -0800

    adding day 5 part 1 solution

---
+++ aoc/solutions/day05/solution.py
@@ -0,0 +1,49 @@
+from aoc.common.timer import timed
+
+type IngredientRange = tuple[int, int]
+type IngredientRanges = list[IngredientRange]
+
+
+@timed
+def part_one(input_txt: str) -> int:
+    ranges = parse_ranges(input_txt)
+
+    ingredients = parse_ingredients(input_txt)
+
+    sum = 0
+
+    for ingredient in ingredients:
+        if is_fresh(ingredient, ranges):
+            sum += 1
+
+    return sum
+
+
+@timed
+def part_two(input_txt: str) -> int:
+    return -1
+
+
+def parse_ranges(input: str) -> IngredientRanges:
+    raw = input.split("\n\n")[0]
+    processed = []
+
+    for row in raw.splitlines():
+        parts = row.split("-")
+        processed.append((int(parts[0]), int(parts[1])))
+
+    return processed
+
+
+def parse_ingredients(input: str) -> list[int]:
+    raw = input.split("\n\n")[1]
+
+    return [int(value) for value in raw.splitlines()]
+
+
+def is_fresh(ingredient: int, ranges: IngredientRanges) -> bool:
+    for start, end in ranges:
+        if ingredient >= start and ingredient <= end:
+            return True
+
+    return False
