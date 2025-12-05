3d3d15f2bdbc4d8f349e5d2dc038a3979052867a
Chance Linz <chancehlinz@gmail.com>
2025-12-05 12:18:57 -0800

    adds day 5 part 2 solution (optimized)

--- aoc/solutions/day05/solution.py
+++ aoc/solutions/day05/solution.py
@@ -20,13 +20,27 @@ def part_one(input_txt: str) -> int:
 @timed
 def part_two(input_txt: str) -> int:
     ranges = parse_ranges(input_txt)
-    fresh_ids = set()
 
-    for start, end in ranges:
-        for i in range(start, end + 1):
-            fresh_ids.add(i)
+    # sort the elements first
+    ranges.sort(key=lambda r: r[0])
 
-    return len(fresh_ids)
+    # use a modified sweep line algorithm for merging the ranges
+    merged_ranges = [ranges[0]]
+    for start, end in ranges[1:]:
+        previous_start, previous_end = merged_ranges[-1]
+        if start <= previous_end:
+            merged_ranges[-1] = (previous_start, max(end, previous_end))
+        else:
+            merged_ranges.append((start, end))
+
+    # sum the ranges (start, =end)
+    # ex: range 3, 5 -> we want to count 3, 4, and 5.
+    fresh_ingredients = 0
+    for start, end in merged_ranges:
+        total_ingredients = (end + 1) - start
+        fresh_ingredients += total_ingredients
+
+    return fresh_ingredients
 
 
 def parse_ranges(input: str) -> list[IngredientRange]:
@@ -52,3 +66,22 @@ def is_fresh(ingredient: int, ranges: list[IngredientRange]) -> bool:
             return True
 
     return False
+
+
+def merge_ranges(ranges: list[IngredientRange]) -> list[IngredientRange]:
+    """
+    A modified version of a sweep line algorithm for efficiently merging ranges of numbers
+
+    example: [(3,5), (10,14), (16,20), (12,18)] becomes [(3,5), (10,20)]
+    """
+    ranges.sort(key=lambda r: r[0])
+    merged_ranges = [ranges[0]]
+
+    for start, end in ranges[1:]:
+        previous_start, previous_end = merged_ranges[-1]
+        if start <= previous_end:
+            merged_ranges[-1] = (previous_start, max(end, previous_end))
+        else:
+            merged_ranges.append((start, end))
+
+    return merged_ranges
