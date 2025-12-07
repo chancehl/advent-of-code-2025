77a76bdb47d28dc69ca7d51efc56686b2b619563
Chance Linz <chancehlinz@gmail.com>
2025-12-07 14:59:27 -0800

    adding day 7 part 2 solution

--- aoc/solutions/day07/solution.py
+++ aoc/solutions/day07/solution.py
@@ -12,7 +12,7 @@ def part_one(input_txt: str) -> int:
             if matrix[row][col] == "." and (
                 matrix[row - 1][col] == "S" or matrix[row - 1][col] == "|"
             ):
-                path = seek_splitter(matrix, (row, col))
+                path = seek_path(matrix, (row, col))
                 draw_beam_path(matrix, path)
             elif matrix[row][col] == "^" and matrix[row - 1][col] == "|":
                 split_beam(matrix, (row, col))
@@ -23,10 +23,36 @@ def part_one(input_txt: str) -> int:
 
 @timed
 def part_two(input_txt: str) -> int:
-    return -1
+    matrix = [list(line) for line in input_txt.splitlines()]
+
+    timeline_counts = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+
+    # always mark S as 1 so we hav something to begin computing our triangle
+    start_row, start_col = (0, matrix[0].index("S"))
+    timeline_counts[start_row][start_col] = 1
+
+    for row in range(0, len(matrix)):
+        for col in range(0, len(matrix[row])):
+            current_count = timeline_counts[row][col]
+
+            if current_count > 0:
+                current_symbol = matrix[row][col]
+
+                if current_symbol == "." or current_symbol == "S":
+                    if is_in_bounds(timeline_counts, (row + 1, col)):
+                        timeline_counts[row + 1][col] += current_count
+                elif current_symbol == "^":
+                    # left
+                    if is_in_bounds(timeline_counts, (row + 1, col - 1)):
+                        timeline_counts[row + 1][col - 1] += current_count
+                    # right
+                    if is_in_bounds(timeline_counts, (row + 1, col + 1)):
+                        timeline_counts[row + 1][col + 1] += current_count
+
+    return sum(timeline_counts[-1])
 
 
-def seek_splitter(
+def seek_path(
     matrix: list[list[str]], starting_posn: tuple[int, int]
 ) -> list[tuple[int, int]]:
     current_row = starting_posn[0]
@@ -56,3 +82,14 @@ def split_beam(matrix: list[list[str]], splitter_posn: tuple[int, int]):
         matrix[row][col - 1] = "|"
     if matrix[row][col + 1] == ".":
         matrix[row][col + 1] = "|"
+
+
+def is_in_bounds(matrix: list[list[int]], posn: tuple[int, int]) -> bool:
+    row, col = posn
+
+    if row < 0 or row > len(matrix) - 1:
+        return False
+    if col < 0 or col > len(matrix[0]) - 1:
+        return False
+
+    return True
